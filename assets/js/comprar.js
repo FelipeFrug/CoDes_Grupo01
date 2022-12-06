ff854cb87755b40e97b47f72402de5c4248da796
FelipeFrug <felipe.frug.mauro@gmail.com>
2022-12-06 20:24:00 -0300

    atualizações

--- assets/js/comprar.js
+++ assets/js/comprar.js
@@ -1,10 +1,3 @@
 document.addEventListener('DOMContentLoaded', function(){
-    let empresas = document.querySelectorAll('.empresas')
-    let compradas = {}
-    for (cia of empresas){
-        cia.addEventListener('click',function(){
-            compradas[cia] = getDate() + getMonth()
-        })
-    }
-    console.log(compradas)
+    localStorage.setItems 
 })
